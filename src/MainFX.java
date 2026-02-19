06e2aa772dea4cc959690346cad53210c2568b14
Rusiru Ransingha <rt.rusiruranasinghe@gmail.com>
2026-02-19 14:03:00 +0530

    Added Initial Commit

---
+++ src/MainFX.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import model.Patient;
+import service.EmergencyQueueSystem;
+
+import java.util.List;
+
+
+public class MainFX extends Application {
+}
