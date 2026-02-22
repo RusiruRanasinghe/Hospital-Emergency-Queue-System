81b54e5ae9103b669300723f9f46f20a58032959
Rusiru Ransingha <rt.rusiruranasinghe@gmail.com>
2026-02-22 15:44:15 +0530

    Update EmergencyQueueSystem.java

---
+++ src/service/EmergencyQueueSystem.java
@@ -0,0 +1,16 @@
+package service;
+
+import ds.DoctorMinHeap;
+import ds.PatientMaxHeap;
+import ds.TreatedHistoryList;
+import engine.PriorityEngine;
+import model.Doctor;
+import model.Patient;
+import util.IdGenerator;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class EmergencyQueueSystem {
+    
+}
